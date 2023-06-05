d7842cbcf067b123e15dc5ac80c761608314f91e
Daniel Lee <jhoon8903@gmail.com>
2023-06-05 19:30:18 +0900

    Common Buff Update

--- Assets/Script/EnemyManagerScript/EnemyManager.cs
+++ Assets/Script/EnemyManagerScript/EnemyManager.cs
@@ -14,12 +14,10 @@ namespace Script.EnemyManagerScript
 
     public class EnemyManager : MonoBehaviour
     {
-        [SerializeField]
-        public List<EnemySettings> enemyList;
+        [SerializeField] public List<EnemySettings> enemyList;
 
         [Header("Boss Prefabs")]
         [SerializeField] public GameObject stage10BossPrefab;  // Stage 10 Boss Prefab
-
         [SerializeField] public GameObject stage20BossPrefab;  // Stage 20 Boss Prefab
 
         public void DecreaseMoveSpeed(int decreaseAmount)
